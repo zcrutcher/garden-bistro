279d951d9c55a188048ab5a10fded83805f4746a
zach <zcrutcher09@gmail.com>
2024-02-02 21:05:03 -0600

    fixed navbar

--- app/layout.js
+++ app/layout.js
@@ -3,11 +3,17 @@ import "./global.scss";
 
 export default function Layout({ children }) {
   return (
-    <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
-      <div className="w-full flex-none md:w-64">
-        <Header />
-      </div>
-      <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
-    </div>
+    <html>
+      <body>
+        <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+          <div className="w-full flex-none md:w-64">
+            <Header />
+          </div>
+          <div className="flex-grow p-6 md:overflow-y-auto md:p-12">
+            {children}
+          </div>
+        </div>
+      </body>
+    </html>
   );
 }
