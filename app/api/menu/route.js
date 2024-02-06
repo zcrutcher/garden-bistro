4c1f3ae62eb781da67faeaa6a645e8760d0976f0
zach <zcrutcher09@gmail.com>
2024-02-05 22:07:02 -0600

    adding null return for api with text/html content type

--- app/api/menu/route.js
+++ app/api/menu/route.js
@@ -1,7 +1,8 @@
 import { NextResponse } from "next/server";
 import { PrismaClient } from "@prisma/client";
 
-export const GET = async () => {
+export const GET = async (req) => {
+  if (req.headers["content-type"] == "text/html") return null;
   const prisma = new PrismaClient();
   const menu = await prisma.menuItem.findMany();
   return NextResponse.json({ results: menu });
--- app/api/reviews/route.js
+++ app/api/reviews/route.js
@@ -2,6 +2,7 @@ import { NextResponse } from "next/server";
 import { PrismaClient } from "@prisma/client";
 
 export const GET = async () => {
+  if (req.headers["content-type"] == "text/html") return null;
   const prisma = new PrismaClient();
   const reviews = await prisma.review.findMany();
   console.log("did I get hit, reviews", reviews);
