9d87222f5ddcdd894ef63ea01c81bf7707f9c145
zach <zcrutcher09@gmail.com>
2024-02-05 22:34:34 -0600

    trying NEXT_PUBLIC_VERCEL_URL instead of VERCEL_URL

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,8 @@
 
 const nextConfig = {
   env: {
-    NEXT_PUBLIC_APP_URL: process.env.VERCEL_URL
-      ? `https://${process.env.VERCEL_URL}`
+    NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_VERCEL_URL
+      ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
       : "http://localhost:3000",
   },
 };
